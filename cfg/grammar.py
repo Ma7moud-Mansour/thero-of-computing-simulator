7ee6cee9d1f52dce5734f9c90fb5026b3a211d93
Omar Hesham <omarhesham1192019@gmail.com>
2025-12-23 16:03:45 +0200

    Clean V2

--- cfg/grammar.py
+++ cfg/grammar.py
@@ -1,7 +1,7 @@
 class Grammar:
     def __init__(self, start):
         self.start = start
-        self.productions = {}  # A -> list of RHS
+        self.productions = {}
 
     def add_production(self, lhs, rhs):
         if lhs not in self.productions:
